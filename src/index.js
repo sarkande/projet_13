a9c1c8842f2c335288f2593fa50f1dc43277e937
sarkande <aperezgo74@gmail.com>
2022-11-28 11:17:54 +0100

    css to sass and html to react

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+import "./styles/index.css";
 
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import SignIn from "./pages/SignIn";
+import User from "./pages/User";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+   //<React.StrictMode>
+   <Router>
+      <Header />
+      <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/signin" element={<SignIn />} />
+         <Route path="/user" element={<User />} />
+         <Route path="*" element={<h1>404: Not Found</h1>} />
+      </Routes>
+      <Footer />
+   </Router>
+   //</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
